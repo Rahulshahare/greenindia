57ebbf083f8cc6d9a87e8cff6db49b2385cf59c7
Rahul Shahare <rahulshahare@gmail.com>
2017-06-19 04:44:04 +0530

    idea-moto added

---
+++ moto.php
@@ -0,0 +1,33 @@
+<?php
+/*
+  The idea behind developing GreenIndia site is that,
+  in upcomming jully maharastra govt is going to plant
+  2cr plants for better envirnment. 
+  Yes ofcourse everyone will be interesting for helping in plantion of those trees. 
+  but after planting those trees who gonna take care of it. 
+  if somebody taking care of planted trees how we gonna know about it. because we cnt visit every tree for 
+  checking its growth. 
+  the prblm is to measure the actual growth of those trees across the state or country. 
+  and with the help of computer science we can have a solution for it. this solition will actully engage the ppls 
+  for protecting and growing the tree. 
+
+  the idea is a login system for every user and a posting box with images and some text. 
+
+  simply a small social network but this will usefull only for managing the trees. 
+
+  if we implimented this model then will can measure the actual growth of any tree planted from anywhere. 
+  we can think about how trees are growing, how ppls are taking part in protecting the tree, or even watering the tree. 
+  or any other problems if arrise. 
+  any time, from anywhere and anyone can see the growth in planted trees and the participation of ppls. 
+
+  so user have to create free account on site. 
+  then he/she have to plant a tree, 
+  for creating a tree in user account the some information will need and it will be 
+    -- location of tree (includes state/district/city)
+    -- the kind of tree/ Name of tree
+    -- Plantation Date 
+    -- and the idea is to create unique name for tree like username
+    -- GPS co-rodinated (if provided)
+    -- some details about tree 
+*/
+?>
